098d6473620c491a79433e8d5bfea6618eec7897
Mounir Zahidi <Mounir.Zahidi-1@ou.edu>
2024-04-09 17:44:07 -0500

    Signup and login user stories done

---
+++ app/src/main/java/com/codepath/zipfix/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.codepath.zipfix
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.parse.ParseObject
+import android.util.Log
+
+
+class MainActivity : AppCompatActivity() {
+
+}
