b4cf226e7b28cd1b6de6a11efb19e876cd0fd83a
SahilTamboli10 <112015150+SahilTamboli10@users.noreply.github.com>
2024-05-02 15:33:15 -0500

    Update Todo.kt

--- app/src/Todo.kt
+++ app/src/Todo.kt
@@ -3,15 +3,16 @@ package np.com.bimalkafle.todoapp
 import java.time.Instant
 import java.util.Date
 
+// Data class representing a Todo item with an id, title, and creation date
 data class Todo(
     var id: Int,
     var title : String,
     var createdAt : Date
 )
 
-
-
+// Function to generate fake Todo items for testing or demonstration purposes
 fun getFakeTodo() : List<Todo>{
+    // Return a list of Todo objects with fake data
     return listOf<Todo>(
         Todo(1,"First todo ",Date.from(Instant.now())),
         Todo(2,"Second todo ",Date.from(Instant.now())),
@@ -19,5 +20,3 @@ fun getFakeTodo() : List<Todo>{
         Todo(4,"this will be my forth todo ",Date.from(Instant.now()))
     );
 }
-
-
