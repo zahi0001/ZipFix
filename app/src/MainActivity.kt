d379d913fe143c8b03fc5b40cf12a3b080e4399f
SahilTamboli10 <112015150+SahilTamboli10@users.noreply.github.com>
2024-05-02 15:32:29 -0500

    MainActivity.kt

--- app/src/MainActivity.kt
+++ app/src/MainActivity.kt
@@ -13,18 +13,23 @@ import np.com.bimalkafle.todoapp.ui.theme.TodoAppTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        
+        // Initialize ViewModel for managing Todo data
         val todoViewModel = ViewModelProvider(this)[TodoViewModel::class.java]
+        
+        // Set the content of the activity using Jetpack Compose
         setContent {
+            // Apply the custom theme to the entire app
             TodoAppTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+                    // Display the Todo list page, passing the ViewModel
                     TodoListPage(todoViewModel)
                 }
             }
         }
     }
 }
-
