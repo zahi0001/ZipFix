3c6e3ef457bfe69df5951723af98d706117d1f7f
SahilTamboli10 <112015150+SahilTamboli10@users.noreply.github.com>
2024-05-02 15:34:09 -0500

    Update TodoListPage.kt

--- app/src/TodoListPage.kt
+++ app/src/TodoListPage.kt
@@ -14,7 +14,6 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -34,33 +33,39 @@ import androidx.compose.ui.unit.sp
 import java.text.SimpleDateFormat
 import java.util.Locale
 
-
+// Composable function representing the TodoListPage
 @Composable
 fun TodoListPage(viewModel: TodoViewModel){
 
+    // Retrieve the todoList from the ViewModel and observe its state
     val todoList by viewModel.todoList.observeAsState()
+    // State variable to hold the input text for adding new todos
     var inputText by remember {
         mutableStateOf("")
     }
 
+    // Column composable to structure the UI vertically
     Column(
         modifier = Modifier
             .fillMaxHeight()
             .padding(8.dp)
     ) {
 
+        // Row composable for input field and add button
         Row(
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(8.dp),
             horizontalArrangement = Arrangement.SpaceBetween
         ) {
+            // Input field for entering new todo text
             OutlinedTextField(
                 modifier= Modifier.weight(1f),
                 value = inputText,
                 onValueChange = {
                     inputText = it
                 })
+            // Button to add new todo
             Button(onClick = {
                 viewModel.addTodo(inputText)
                 inputText = ""
@@ -69,9 +74,11 @@ fun TodoListPage(viewModel: TodoViewModel){
             }
         }
 
+        // LazyColumn to display the list of todos
         todoList?.let {
             LazyColumn(
                 content = {
+                    // Iterate through todoList and display each todo item
                     itemsIndexed(it){ index: Int, item: Todo ->
                         TodoItem(item = item, onDelete = {
                             viewModel.deleteTodo(item.id)
@@ -80,19 +87,19 @@ fun TodoListPage(viewModel: TodoViewModel){
                 }
             )
         }?: Text(
+            // Displayed when todoList is null or empty
             modifier = Modifier.fillMaxWidth(),
             textAlign = TextAlign.Center,
             text = "No items yet",
             fontSize = 16.sp
         )
-
-
     }
-
 }
 
+// Composable function representing a single Todo item
 @Composable
 fun TodoItem(item : Todo,onDelete : ()-> Unit) {
+    // Row composable to structure the todo item horizontally
     Row(
         modifier = Modifier
             .fillMaxWidth()
@@ -101,22 +108,25 @@ fun TodoItem(item : Todo,onDelete : ()-> Unit) {
             .background(MaterialTheme.colorScheme.primary)
             .padding(16.dp),
         verticalAlignment = Alignment.CenterVertically
-
     ) {
+        // Column composable to structure the content of the todo item vertically
         Column(
             modifier = Modifier.weight(1f)
         ) {
+            // Display the creation date of the todo item
             Text(
                 text = SimpleDateFormat("HH:mm:aa, dd/mm", Locale.ENGLISH).format(item.createdAt),
                 fontSize = 12.sp,
                 color = Color.LightGray
             )
+            // Display the title of the todo item
             Text(
                 text = item.title,
                 fontSize = 20.sp,
                 color = Color.White
             )
         }
+        // IconButton for deleting the todo item
         IconButton(onClick = onDelete) {
             Icon(
                 painter = painterResource(id = R.drawable.baseline_delete_24),
@@ -126,19 +136,3 @@ fun TodoItem(item : Todo,onDelete : ()-> Unit) {
         }
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
