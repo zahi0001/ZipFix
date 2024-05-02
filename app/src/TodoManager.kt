adb42d635f317842f552ad7fbc27ddea7cfb927a
SahilTamboli10 <112015150+SahilTamboli10@users.noreply.github.com>
2024-05-02 15:34:47 -0500

    Update TodoManager.kt

--- app/src/TodoManager.kt
+++ app/src/TodoManager.kt
@@ -3,24 +3,32 @@ package np.com.bimalkafle.todoapp
 import java.time.Instant
 import java.util.Date
 
+// Singleton object responsible for managing Todo items
 object TodoManager {
 
+    // Mutable list to store Todo items
     private val todoList = mutableListOf(
         Todo(1, "Upstairs carpet needs to be replaced", Date.from(Instant.now()))
     )
 
-
+    // Function to retrieve all Todo items
     fun getAllTodo() : List<Todo>{
         return todoList
     }
 
+    // Function to add a new Todo item
     fun addTodo(title : String){
-        todoList.add(Todo(System.currentTimeMillis().toInt(),title, Date.from(Instant.now())))
+        // Generate a unique id for the new Todo item based on current system time
+        val id = System.currentTimeMillis().toInt()
+        // Create a new Todo item and add it to the list
+        todoList.add(Todo(id, title, Date.from(Instant.now())))
     }
 
+    // Function to delete a Todo item by its id
     fun deleteTodo(id : Int){
+        // Remove the Todo item from the list if its id matches the provided id
         todoList.removeIf{
-            it.id==id
+            it.id == id
         }
     }
 
